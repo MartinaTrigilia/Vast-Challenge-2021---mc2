7605cca0584b868a828d22d41786cb7b6d7b9819
MartinaTrigilia <trigilia.martina96@gmail.com>
2023-01-16 00:11:59 +0100

    added kronos island to map

---
+++ src/components/AbilaMap.vue
@@ -0,0 +1,84 @@
+<template>
+
+  <div style="height: 350px;">
+    <div class="info" style="height: 15%">
+      <span>Center: {{ center }}</span>
+      <span>Zoom: {{ zoom }}</span>
+      <span>Bounds: {{ bounds }}</span>
+    </div>
+    <l-map
+        style="height: 500px"
+        :zoom="zoom"
+        :maxZoom="maxZoom"
+        :minZoom="minZoom"
+        :center="center"
+        @update:zoom="zoomUpdated"
+        @update:center="centerUpdated"
+        @update:bounds="boundsUpdated"
+    >
+      <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+      <l-geo-json :geojson="geojsonIsl" :options-style="styleFunction"></l-geo-json>
+
+      <l-geo-json :geojson="geojson" :options-style="styleFunction"></l-geo-json>
+
+    </l-map>
+  </div>
+</template>
+
+<script>
+import {LMap, LTileLayer,LGeoJson} from 'vue2-leaflet';
+
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+    LGeoJson
+  },
+  data () {
+    return {
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      attribution:
+          '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      zoom: 13,
+      maxZoom:15,minZoom:7,
+      center: [36.069082, 24.867382],
+      bounds: null,
+      geojson: null,
+      geojsonIsl: null
+
+
+    };
+  },
+  async created() {
+    const response = await fetch("/json/Abila.json");
+    const response2 = await fetch("/json/Kronos_Island.json");
+    const data = await response.json();
+    const data2 = await response2.json();
+    this.geojsonIsl = data2;
+    this.geojson = data;
+  },
+  methods: {
+    zoomUpdated (zoom) {
+      this.zoom = zoom;
+    },
+    centerUpdated (center) {
+      this.center = center;
+    },
+    boundsUpdated (bounds) {
+      this.bounds = bounds;
+    }
+  },
+  computed: {
+    styleFunction() {
+      return () => {
+        return {
+          weight: 1,
+          color: "rgba(105,105,105,0.94)",
+          opacity: 1,
+          pane: 'mapPane',
+        };
+      };
+    }
+  }
+}
+</script>
